c426116c2542824d31420272b7235395ca02f94c
Shishir-Ashok <shishirashok17@gmail.com>
2025-03-29 23:16:06 +0000

    sign-in, sign-up, jwt functionalities

---
+++ models/user.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const { model, Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const UserModel = model('User', userSchema);
+module.exports = UserModel;
